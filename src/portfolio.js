e8e5c67e998ed6e6b10bd5b3d911d972457e3f63
MichaelHulme <coachhulmeumb@gmail.com>
2022-02-23 22:31:39 -0500

    updated about section

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,11 +1,16 @@
 import {React, useState} from 'react';
 import './portfolio.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faReact, faCss3Alt, faNode, faHtml5, faJs, faBootstrap} from '@fortawesome/free-brands-svg-icons'
-import { faClose} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faReact, faCss3Alt, faNode, faHtml5, faJs, faBootstrap, faGitAlt, faNodeJs} from '@fortawesome/free-brands-svg-icons'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 import zenCircle from '/Users/michaelhulme/projects/porfolio/src/images/greyZenCircle.png'
+import albert from '/Users/michaelhulme/projects/porfolio/src/images/albert.png'
+import mentalHealth from '/Users/michaelhulme/projects/porfolio/src/images/mentalHealth.png'
+import coaching from '/Users/michaelhulme/projects/porfolio/src/images/tf.png'
+import robot from '/Users/michaelhulme/projects/porfolio/src/images/robotWhite.png'
 
 export default function Portfolio(){
+    const [displayedAboutItem, setDisplayedAboutItem] = useState()
     const [displayedProject, setDisplayedProject] = useState()
     const [isProjectDisplayed, setIsProjectDisplayed] = useState(false)
     const [zenCircleRotationDirection, setZenCircleRoationDirection] = useState()
@@ -34,8 +39,133 @@ export default function Portfolio(){
         }, 2000)
     }
 
-    const emptyContent = (
-        <div className="hidden"></div>
+    const handleDisplayAboutItem = (displayedItem) => {
+        let aboutItems = document.querySelectorAll('.about-item')
+        document.querySelector('.paragraph-with-backdrop-boxshadow').classList.add('mute')
+
+        aboutItems.forEach(item => {
+            item.classList.add('mute')
+        }) 
+        
+        setTimeout(() => {
+            setDisplayedAboutItem(displayedItem)
+        }, 500)
+    }
+
+    const handleUndisplayAboutItem = () => {
+        let aboutItems = document.querySelectorAll('.about-item')
+        document.querySelector('.project-content').classList.add('move-down')
+
+        setTimeout(() => {
+            document.querySelector('.paragraph-with-backdrop-boxshadow').classList.remove('mute')
+            aboutItems.forEach(item => {
+                item.classList.remove('mute')
+            }) 
+            setDisplayedAboutItem('')
+        }, 2000)
+    }
+
+    const educationContent = (
+        <div className="project-content">
+            <FontAwesomeIcon className="project-close-icon" onClick={() => {handleUndisplayAboutItem()}}icon={faClose} />
+            <div className="school">
+                <div className="underlined-title">University of North Carolina, Wilmington</div>
+                <div className="sub-title">Bachelor of Science in Applied Mathematics</div>
+                <div className="list-title">Area's of Study:</div>
+                <ol id="studies-list">
+                    <li>Fermat's Last Theorem for the case n=3</li>
+                    <li>Partial Differential Equations</li>
+                    <li>Vector Calculus</li>
+                    <li>Abstract Algebra</li>
+                </ol> 
+            </div>
+            <div className="school">
+                <div className="underlined-title">Bridgewater State University</div>
+                <div className="sub-title">Master of Science in Strength and Conditioning</div>
+                
+                <div className="list-title">Area's of Study:</div>
+                <ol id="studies-list">
+                    <li>Olympic Lifting for College Athletes</li>
+                    <li>Rest and Recovery in Athletic Performance</li>
+                    <li>Nutrition</li>
+                    <li>Biomechanics of Sprinting</li>
+                </ol> 
+            </div>
+        </div>
+    )
+
+    const technologiesContent = (
+        <div className="project-content">
+            <FontAwesomeIcon className="project-close-icon" onClick={() => {handleUndisplayAboutItem()}}icon={faClose} />
+            <div className="technologies-container">
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faHtml5}/>
+                    <div className="regular-paragraph">html</div>
+                </div>
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faCss3Alt}/>
+                    <div className="regular-paragraph">css</div>
+                </div>
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faJs}/>
+                    <div className="regular-paragraph">javascript</div>
+                </div>
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faReact}/>
+                    <div className="regular-paragraph">react</div>
+                </div>
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faGitAlt}/>
+                    <div className="regular-paragraph">github</div>
+                </div>
+                <div className="tech-container">
+                    <FontAwesomeIcon className="tech-icon" icon={faNodeJs}/>
+                    <div className="regular-paragraph">nodejs</div>
+                </div>    
+            </div>
+        </div>
+    )
+
+    const mentalHealthContent = (
+        <div className="project-content">
+            <FontAwesomeIcon className="project-close-icon" onClick={() => {handleUndisplayAboutItem()}}icon={faClose} />
+            <div className="school">
+                <div className="underlined-title">Cohannet Academy</div>
+                <div className="sub-title">Residential Supervisor 2014-2022</div>
+                <div className="paragraph-title">Transferable Skills:</div>
+                <div className="regular-paragraph-left">communication, pattern recognition</div>
+                <div className="paragraph-title">Job Description:</div>
+                <div className="regular-paragraph-left">
+                    All things related to running an Intensive Residential Treatment Program for teenage girls who struggle with suicidal ideation, eating disorders, or other self injurious behavior. My primary responsibilities were to manage the staff on shift, intervine when a resident is in crisis, and facilitate communication between the various departments in the program.  
+                    <br/>
+                    <br/>
+                    The ability to identify patterns of behavior in the residents was crucial to managing their behavior. While the ability collaborate effectively accross deparrments greatly improved the efficay of our treatment plans.
+                </div>   
+            </div>
+        </div>
+    )
+
+    const coachingContent = (
+        <div className="project-content">
+            <FontAwesomeIcon className="project-close-icon" onClick={() => {handleUndisplayAboutItem()}}icon={faClose} />
+            <div className="school">
+                <div className="underlined-title-small">Bryant University</div>
+                <div className="sub-title">Assistant Track and Field Coach 2018-2019</div>
+                <div className="underlined-title-small">Bridgewater State University </div>
+                <div className="sub-title">Assistant Track and Field Coach 2014-2018</div>
+                <div className="underlined-title-small">University of Massachusetts, Boston</div>
+                <div className="sub-title">Assistant Track and Field Coach 2012-2014</div>
+                <div className="paragraph-title">Transferable Skills:</div>
+                <div className="regular-paragraph-left">communication, pattern recognition</div>
+                <div className="paragraph-title">Job Description:</div>
+                <div className="regular-paragraph-left">
+                    All things related to running an Intensive Residential Treatment Program for teenage girls who struggle with suicidal ideation, eating disorders, or other self injurious behavior. My primary responsibilities were to manage the staff on shift, intervine when a resident is in crisis, and facilitate communication between the various departments in the program.  
+                    <br/>
+                    <br/>
+                    The ability to identify patterns of behavior in the residents was crucial to managing their behavior. While the ability collaborate effectively accross deparrments greatly improved the efficay of our treatment plans.
+                </div>   
+            </div>
+        </div>
     )
 
     const littleLegumesContent = (
@@ -54,8 +184,8 @@ export default function Portfolio(){
                 <FontAwesomeIcon className="project-tech-icon" icon={faGithub}/>
             </div>
             <div className="btn-container">
-                <button className="btn">Visit Site</button>
-                <button className="btn">See Code</button>
+                <a href="https://little-legumes.herokuapp.com/" target="_blank" className="btn">Visit Site</a>
+                <a href="https://github.com/mikey137/Little_Legumes_App" target="_blank" className="btn">View Code</a>
             </div>
         </div>
     )
@@ -75,8 +205,8 @@ export default function Portfolio(){
                 <FontAwesomeIcon className="project-tech-icon" icon={faGithub}/>
             </div>
             <div className="btn-container">
-                <button className="btn">Visit Site</button>
-                <button className="btn">See Code</button>
+                <a href="http://supercardsnft.herokuapp.com/" target="_blank" className="btn">Visit Site</a>
+                <a href="https://github.com/Ben8842/asg" target="_blank" className="btn">View Code</a>
             </div>
         </div>
     )
@@ -98,8 +228,8 @@ export default function Portfolio(){
                 <FontAwesomeIcon className="project-tech-icon" icon={faGithub}/>
             </div>
             <div className="btn-container">
-                <button className="btn">Visit Site</button>
-                <button className="btn">See Code</button>
+                <a href="https://mikey137.github.io/EmotionsAPI/index.html" target="_blank" className="btn">Visit Site</a>
+                <a href="https://github.com/mikey137/EmotionsAPI" target="_blank" className="btn">View Code</a>
             </div>
         </div>
     )
@@ -120,6 +250,28 @@ export default function Portfolio(){
                     <div className="section-title">
                         about
                     </div>
+                    <div className="project-container">
+                        <div className="paragraph-with-backdrop-boxshadow">
+                            I'm in the process of tranistioning from a career in mental health to one as a software developer. I have recently completed a boot camp and I'm continuing to build my skill set by taking an advanced javascript course, as well as wokring on an NFT project. My ideal position would be one where I can learn from someone with more experience, but also be given personal resposiblities and be expected to complete tasks independently. 
+                        </div>
+                        <div className="about-item" onClick={() => handleDisplayAboutItem(educationContent)}>
+                            <img className = "about-img" src={albert} alt="albert" />
+                            education
+                        </div>
+                        <div className="about-item" onClick={() => handleDisplayAboutItem(technologiesContent)}>
+                            <img className = "about-img" src={robot} alt="robot" />
+                            technologies used
+                        </div>
+                        <div className="about-item" onClick={() => handleDisplayAboutItem(mentalHealthContent)}>
+                            <img className = "about-img" src={mentalHealth} alt="mental health" />
+                            mental health
+                        </div>
+                        <div className="about-item" onClick={() => handleDisplayAboutItem(coachingContent)}>
+                            <img className = "about-img" src={coaching} alt="coaching" />
+                            coaching
+                        </div>
+                    </div>
+                    {displayedAboutItem}
                 </div>
                 <div className="section projects">
                     <div className="section-title">
