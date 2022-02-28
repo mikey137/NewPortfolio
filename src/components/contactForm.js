a6920410d380e7ad0184885a033e09f9d40d3b8e
MichaelHulme <coachhulmeumb@gmail.com>
2022-02-28 13:10:19 -0500

    made contact form functional

---
+++ src/components/contactForm.js
@@ -0,0 +1,126 @@
+import {React, useState} from 'react';
+import '../portfolio.css'
+import {useForm} from 'react-hook-form'
+import emailjs from 'emailjs-com'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+
+export default function ContactForm() {
+    const [disabled, setDisabled] = useState(false)
+    const [isMessageSent, setIsMessageSent] = useState(false)
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        const { name, email, subject, message } = data;
+        try {
+          setDisabled(true);
+    
+          const templateParams = {
+            name,
+            email,
+            subject,
+            message
+          };
+          await emailjs.send(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            templateParams,
+            process.env.REACT_APP_USER_ID
+          );
+          reset();
+          setIsMessageSent(true)
+          setDisabled(false);
+          setTimeout(() => {
+            setIsMessageSent(false)
+          },6100)
+        } catch (e) {
+          console.log(e);
+        }
+      };
+
+    return(
+        <div className="contact-form-wrapper">
+            <form 
+                className="contact-form"
+                onSubmit={handleSubmit(onSubmit)}
+                noValidate
+            >
+                <input 
+                    type="text" 
+                    className="form__input" 
+                    name="name" 
+                    placeholder="Full name" 
+                    {...register('name', {
+                        required: {
+                          value: true,
+                          message: 'Please enter your name'
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                    })}
+                />
+                {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                <input 
+                  type="email" 
+                  className="form__input" 
+                  name="email" 
+                  placeholder="Email" 
+                  {...register('email', {
+                    required: true,
+                    pattern:
+                      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                  })} 
+                />
+                {errors.email && (
+                      <span className='errorMessage'>Please enter a valid email address</span>
+                )}
+                <input 
+                  type="text" 
+                  className="form__input" 
+                  name="subject" 
+                  placeholder="Subject" 
+                  {...register('subject', {
+                    required: {
+                      value: true,
+                      message: 'Please enter a subject'
+                    },
+                    maxLength: {
+                      value: 75,
+                      message: 'Subject cannot exceed 75 characters'
+                    }
+                  })}
+                />
+                {errors.subject && (
+                      <span className='errorMessage'>{errors.subject.message}</span>
+                )}
+                <textarea 
+                  className="form__input field" 
+                  name="message" 
+                  placeholder="Write something.." 
+                  {...register('message', {
+                    required: true
+                  })}
+                ></textarea>
+                {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                <button  type="submit" className="btn" disabled={disabled}>Submit</button>
+            </form>
+            <div className="icons-container">
+                <a target="_blank" rel="noreferrer" href="https://github.com/mikey137">
+                    <FontAwesomeIcon className='social-media-icon' icon={ faGithub }/>
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-hulme-63b81a214/">
+                    <FontAwesomeIcon className='social-media-icon' icon={ faLinkedin }/>
+                </a>
+            </div>
+            <div className={ isMessageSent ? "message-sent-alert" : "hidden" }>Message Sent!</div>
+        </div>
+    )
+}
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,6 @@
 import {React, useState} from 'react';
 import './portfolio.css'
+import ContactForm from './components/contactForm';
 import { Link, Element } from 'react-scroll'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faReact, faCss3Alt, faNode, faHtml5, faJs, faBootstrap, faGitAlt, faNodeJs} from '@fortawesome/free-brands-svg-icons'
@@ -17,7 +18,7 @@ export default function Portfolio(){
     const [displayedProject, setDisplayedProject] = useState()
     const [isProjectDisplayed, setIsProjectDisplayed] = useState(false)
     const [zenCircleRotationDirection, setZenCircleRoationDirection] = useState()
-    
+        
     const handleDisplayProject = (project) => {
         setZenCircleRoationDirection('rotate-clockwise')
 
@@ -368,21 +369,7 @@ export default function Portfolio(){
                         contact
                     </div>
                     <p className="title-sub-text">email me! maybe?</p>
-                    <div className="contact-form">
-                        <input type="text" className="form__input" id="name" placeholder="Full name" required="" />
-                        <input type="text" className="form__input" id="email" placeholder="Email" required="" />
-                        <input type="text" className="form__input" id="subject" placeholder="Subject" required="" />
-                        <textarea id="field" className="form__input" name="subject" placeholder="Write something.."></textarea>
-                        <button className="btn">Submit</button>
-                    </div>
-                    <div className="icons-container">
-                        <a target="_blank" rel="noreferrer" href="https://github.com/mikey137">
-                            <FontAwesomeIcon className='social-media-icon' icon={ faGithub }/>
-                        </a>
-                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-hulme-63b81a214/">
-                            <FontAwesomeIcon className='social-media-icon' icon={ faLinkedin }/>
-                        </a>
-                    </div>
+                    <ContactForm></ContactForm>
                 </Element>
             </div>
         </div>
